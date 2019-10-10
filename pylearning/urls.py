e9dd0c18bb324eedfe6a4002a8338be68c032cfb
Harjinder Singh <harjinderfcsi@gmail.com>
2019-10-10 00:47:16 -0400

    Updated UI

--- pylearning/urls.py
+++ pylearning/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('python.urls')),
     path('accounts/', include('accounts.urls')),
     path('gallary/', include('gallary.urls')),
+    path('', views.index, name="index"),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
